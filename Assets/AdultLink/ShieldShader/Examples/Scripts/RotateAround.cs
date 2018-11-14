c79544054812e392a5b51077fb6a3edf7e9f5e75
Willy Angel <gguillermix@gmail.com>
2018-11-14 19:57:10 +0100

    "Photoshoot" settings

---
+++ Assets/AdultLink/ShieldShader/Examples/Scripts/RotateAround.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AdultLink {
+public class RotateAround : MonoBehaviour {
+	public Transform pivotPoint;
+	public float rotationSpeed = -500f;
+	public Vector3 axis;
+	// Update is called once per frame
+
+	void Update () {
+		transform.RotateAround(pivotPoint.transform.position, axis, rotationSpeed*Time.deltaTime);
+	}
+}
+}
