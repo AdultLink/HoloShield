1119f826e129b91eb3ec3bc7c9691a1e3931335f
Willy Angel <gguillermix@gmail.com>
2018-11-16 16:39:05 +0100

    Added barbarian distortion example, needs tweaking. Disabled for now.

---
+++ Assets/AdultLink/ShieldShader/Examples/Scripts/animationController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animationController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Animator anim;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q)) {
+            anim.SetTrigger("activateTransition");
+        }
+
+        if (Input.GetKeyDown(KeyCode.E)) {
+            anim.SetTrigger("activateTransition_inv");
+        }
+    }
+}
