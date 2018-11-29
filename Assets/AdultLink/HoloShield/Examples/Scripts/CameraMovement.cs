777504ceefbe4e6bd96557b4eb966edb78312e4f
Willy Angel <gguillermix@gmail.com>
2018-11-29 18:18:30 +0100

    Heavy renaming

---
+++ Assets/AdultLink/HoloShield/Examples/Scripts/CameraMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+
+	// Use this for initialization
+	public float speed = 0.1f;
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.D)) {
+		transform.Translate(speed,0f,0f);
+		}
+		if (Input.GetKey(KeyCode.A)) {
+		transform.Translate(-speed,0f,0f);
+		}
+	}
+}
